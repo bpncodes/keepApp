f929f8fe2560c90b4618e44b29d1d3d26bc07aee
bpncodes <131p11v@gmail.com>
2020-09-01 17:28:24 +0530

    fixed fonts

---
+++ build/precache-manifest.1c5af135121e087fbfa025c73597a234.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fa6280464967f076cfc9419e9852df5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f2e9c83d43940adb6b7",
+    "url": "/static/js/2.3c966012.chunk.js"
+  },
+  {
+    "revision": "489b1a8565cd31a9264f",
+    "url": "/static/js/main.0c08baf4.chunk.js"
+  },
+  {
+    "revision": "05c790a9a60e4d4b4be2",
+    "url": "/static/js/runtime-main.a8757f30.js"
+  }
+]);
